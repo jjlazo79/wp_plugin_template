8adf8d5d7f89a63de676d0b2786d947fd28814c5
Jose Lazo <jjlazo79@gmail.com>
2015-11-06 20:50:12 +0100

    Update wp_plugin_template.php

--- wp_plugin_template.php
+++ wp_plugin_template.php
@@ -1,28 +1,12 @@
 <?php
 /*
 Plugin Name: WP Plugin Template
-Plugin URI: https://github.com/fyaconiello/wp_plugin_template
+Plugin URI: https://github.com/jjlazo79/wp_plugin_template
 Description: A simple wordpress plugin template
 Version: 1.0
-Author: Francis Yaconiello
-Author URI: http://www.yaconiello.com
+Author: Jose Lazo
+Author URI: http://www.joselazo.es
 License: GPL2
-*/
-/*
-Copyright 2012  Francis Yaconiello  (email : francis@yaconiello.com)
-
-This program is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License, version 2, as
-published by the Free Software Foundation.
-
-This program is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with this program; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA  02110-1301  USA
 */
 
 if(!class_exists('WP_Plugin_Template'))
